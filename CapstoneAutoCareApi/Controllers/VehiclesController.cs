847e8d72255f5589473429b4d4a9cc03ebfa698e
xuanDuy <xuanduy650@gmail.com>
2024-05-23 18:59:24 +0700

    commit

---
+++ CapstoneAutoCareApi/Controllers/VehiclesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Application;
+using Domain.Entities;
+
+namespace CapstoneAutoCareApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public VehiclesController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehicles>>> GetVehicles()
+        {
+            return await _context.Vehicles.ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Vehicles>> GetVehicles(Guid id)
+        {
+            var vehicles = await _context.Vehicles.FindAsync(id);
+
+            if (vehicles == null)
+            {
+                return NotFound();
+            }
+
+            return vehicles;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutVehicles(Guid id, Vehicles vehicles)
+        {
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Vehicles>> PostVehicles(Vehicles vehicles)
+        {
+            _context.Vehicles.Add(vehicles);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehicles", new { id = vehicles.VehiclesId }, vehicles);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteVehicles(Guid id)
+        {
+
+            return NoContent();
+        }
+
+        
+    }
+}
